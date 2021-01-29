b4092e5162b5ebf7b8b012b9eec9844c3fd33836
Rémi Cohen-Scali <rcoscali@users.noreply.github.com>
2021-01-30 00:48:21 +0100

    Update build-n-test.sh

--- build-n-test.sh
+++ build-n-test.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# First install Test::Pod
+sudo cpan -f -i Test::Pod
+# Then Test::Pod::Coverage
+sudo cpan -f -i Test::Pod::Coverage
+# Cd to json_minify directory
+cd json_minify
+# and generate Makefile
 perl Makefile.PL
+# Make
 make all
+# And make tests
 make test
